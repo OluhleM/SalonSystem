7d3882284ff9a93c001ae10730e037fc15c62d58
ZintleMgqongose <137223926+ZintleMgqongose@users.noreply.github.com>
2025-05-23 19:25:00 +0200

    Payment Page
    I have started to code payment page

---
+++ App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import PaymentPage from './components/PaymentPage'; // Correct import path
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <PaymentPage />
+    </div>
+  );
+}
+
+export default App;
