e4c8db242c30e1c09db75f5b2db6ad7c583fe096
OluhleM <222419636@mycput.ac.za>
2025-08-19 20:48:51 +0200

    I made plenty of changes to the backend and shifted the structure.

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,6 +1,7 @@
 const jwt = require("jsonwebtoken");
 const User = require("../models/User");
 
+// Authenticate user and attach user object to request
 const protect = async (req, res, next) => {
     let token;
 
@@ -11,15 +12,33 @@ const protect = async (req, res, next) => {
 
             req.user = await User.findById(decoded.id).select("-password");
 
+            if (!req.user) {
+                return res.status(401).json({ message: "User not found" });
+            }
+
             next();
         } catch (err) {
-            res.status(401).json({ message: "Not authorized, token failed" });
+            console.error(err);
+            return res.status(401).json({ message: "Not authorized, token failed" });
         }
     }
 
     if (!token) {
-        res.status(401).json({ message: "Not authorized, no token" });
+        return res.status(401).json({ message: "Not authorized, no token" });
     }
 };
 
-module.exports = { protect };
+// Authorize based on user roles
+const authorizeRoles = (...roles) => {
+    return (req, res, next) => {
+        if (!req.user || !roles.includes(req.user.role)) {
+            return res.status(403).json({
+                message: `Access denied: requires one of [${roles.join(", ")}]`
+            });
+        }
+        next();
+    };
+};
+
+module.exports = { protect, authorizeRoles };
+
